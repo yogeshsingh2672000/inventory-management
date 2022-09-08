c9dd30c813fd4803153943fa5173a09af6c35fa3
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2022-09-08 18:44:38 +0530

    removed console log

--- routes/auth.js
+++ routes/auth.js
@@ -17,7 +17,6 @@ router.post(
     }),
   ],
   async (req, res) => {
-    console.log(req.body);
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
